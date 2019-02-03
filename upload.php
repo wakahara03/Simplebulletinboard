6912056eca70a094a5d1a8884b0cf60ecc70c9d0
wakahara03 <46309168+wakahara03@users.noreply.github.com>
2019-02-03 20:52:43 +0900

    Update upload.php

--- upload.php
+++ upload.php
@@ -3,17 +3,14 @@
 if(!empty($file)){
  
     $upload_path = 'images/'.$file['name']; 
-    $rst = move_uploaded_file($file['tmp_name'],$upload_path); 
+    $upload_file = move_uploaded_file($file['tmp_name'],$upload_path); 
  
-    if ($rst){
+    if (!empty($upload_file)){
         $msg = '画像をアップロードしました！';
         $img_path = $upload_path; 
     }else{
-        $msg = '画像はアップロード出来ませんでした。';
+        $msg = '画像を選択してください';
     }
-
-}else{
-  $msg = '画像を選択してください';
 }
  
 ?>
